6c436dfdc98b5918e959a94ed8c9cefb6cc540f9
John Lagoutaris <jjlagoutaris@gmail.com>
2022-02-19 11:21:02 -0500

    I have added the Library as a table and implemented basic buttons whose functionality I am now working on

--- library.js
+++ library.js
@@ -1,6 +1,9 @@
 const userLibrary = [];
-let libraryContainer = document.querySelector('.library-container');
-let book = document.querySelectorAll('.book');
+let libraryContainer = document.querySelector('#library');
+let addBtn = document.querySelector('.add-book');
+let updateBtn = document.querySelector('.update-progress');
+let removeBooksBtn = document.querySelector('.remove-selected-books');
+let emptyLibraryBtn = document.querySelector('.empty-library');
 
 function Book(title, author, pages, progress){
 
@@ -11,25 +14,51 @@ function Book(title, author, pages, progress){
 
 }
 
-hg = new Book("HG", "SC", 300, "Read");
-hg2 = new Book("CF", "SC", 350, "Not Read");
+hg = new Book("Hunger Games", "Suzanne Collins", 300, "Read");
+hg2 = new Book("Hunger Games: Catching Fire", "Suzanne Collins", 350, "Read");
 
-addBookToLibrary(hg);
-addBookToLibrary(hg2);
+
+
+// addBookToLibrary(hg);
+// addBookToLibrary(hg2);
 
 function addBookToLibrary(bookObj){
     userLibrary.push(bookObj);
+
+    let row = document.createElement("tr");
+    row.className = 'row';
+
+    let title = document.createElement("td");
+    title.className = 'book-cell';
+    title.textContent = bookObj.title;
+
+    let author = document.createElement("td");
+    author.className = 'book-cell';
+    author.textContent = bookObj.author;
+
+    let pages = document.createElement("td");
+    pages.className = 'book-cell';
+    pages.textContent = bookObj.pages;
+
+    let progress = document.createElement("td");
+    progress.className = 'book-cell';
+    progress.textContent = bookObj.progress;
+
+    
+    row.appendChild(title);
+    row.appendChild(author);
+    row.appendChild(pages);
+    row.appendChild(progress);
+    libraryContainer.appendChild(row);
 }
 
-// console.log(userLibrary);
 
+// function viewLibraryBooks(){
+//     for (let i = 0; i < userLibrary.length; i++){
+//         for(libraryBook in userLibrary[i]){
+//             book.textContent += `${libraryBook}: ${userLibrary[i][libraryBook]} \n`;
+//         }
+//     }
+// }
 
-function viewLibraryBooks(){
-    for (let i = 0; i < userLibrary.length; i++){
-        for(libraryBook in userLibrary[i]){
-            book.textContent += `${libraryBook}: ${userLibrary[i][libraryBook]} \n`;
-        }
-    }
-}
-
-viewLibraryBooks();
+// viewLibraryBooks();
